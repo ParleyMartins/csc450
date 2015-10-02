c4327b759a10bdcaea3b68c47991bae69ac9284d
Parley Martins <parleypachecomartins@gmail.com>
2015-10-02 16:12:41 +0000

    Add new stuff to this file

--- assignment/teste.cpp
+++ assignment/teste.cpp
@@ -1,32 +1,63 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <random>
+#include <ctime>
+
+#define TRAINING_INPUT_SIZE 10
+#define TRAINING_SAMPLE_SIZE 10
 
 using namespace std;
 
-int give_label(float training_data[]){
+int wibble_classificator(float training_data[]){
 	int array_size = sizeof(training_data);
 	int r[] = {0, 1};
 	float label = 0;
-	cout << "array size " << array_size << endl;
 	for(int i = 0; i < array_size; i++){
 		label += (training_data[i] * r[i%2]);
-		cout << "td: " << training_data[i] << endl;
-		cout << "i%2: " << i%2 << " r[i%2]: " << r[i%2]  << endl;
-		cout << "label: " << label << endl;
 	}
-	return round(label);
+	return label > 1;
 }
 
+float* generate_training_inputs(){
+	srand(time(NULL));
+	float training_input[TRAINING_INPUT_SIZE];
+	for(int i = 0; i < TRAINING_INPUT_SIZE; i++){
+		float input = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		training_input[i] = input;
+	}
+	return training_input;
+}
+
+void generate_noise(){
+	//For each one of the training samples add some noise
+}
+
+void generate_input_weights(){
+	//Randomly generate the weights, with numbers near to zero.
+}
+
+void calculate_nodes(){
+	//Generate with the sigmoid formula 
+}
+
+void generate_nodes_weights(){
+	//Also random numbers, near to zero
+}
+
+void calculate_guess_label(){
+	//Calculate with the sigmoid formula
+}
+
+void calculate_delta(){}
+
+void update_weights(){}
+
+//Return the weights
+
 int main(){
-	float test[] = {0.7,
--0.8,
-0.9,
-0.631,
--0.842,
-0.861,
-0.976,
--0.784};
-	cout << give_label(test) << endl;
+	for(int i = 0; i < TRAINING_INPUT_SIZE; i++){
+		cout << wibble_classificator(generate_training_inputs()) << endl;
+	}
 	return 0;
 }
